9424f3a4ebacc5d9ec0d08ae752f6a1f6ba84ccb
Meghraj Jare <meghrajjare77@gmail.com>
2025-03-05 18:42:31 +0530

    Optimizing Module Imports

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        // protocol: "https",
-        hostname: "ogopptndxf9vyzxi.public.blob.vercel-storage.com",
-        // pathname: "/[username]",
-      },
-    ],
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
   },
 };
 
