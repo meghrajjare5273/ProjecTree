a17b35e9e4dabfb26526494d5c3d4158b125b6b0
Meghraj Jare <meghrajjare77@gmail.com>
2025-03-11 00:05:29 +0530

    Updated Repository

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import { Poppins, Inter } from "next/font/google";
 import Image from "next/image";
